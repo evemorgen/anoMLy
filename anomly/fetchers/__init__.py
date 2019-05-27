cae7f682f1c63acacb47b6b497467a700d557ded
Patryk Galczynski <galczynski.patryk@gmail.com>
2019-05-28 00:08:40 +0200

    update fetchers init

--- anomly/fetchers/__init__.py
+++ anomly/fetchers/__init__.py
@@ -4,8 +4,10 @@ from .elastic import ElasticPathsFetcher
 from .metric_fetcher import MetricFetcher
 from .metric_fetcher_to_file import MetricFetcherToFile
 from .sarima_model_fetcher import SarimaModelFetcher
+from .soia_metrics_fetcher import SoiaMetricsFetcher
 
 __all__ = [
     'StaticJSON', 'SoiaEmailFetcher', 'ElasticPathsFetcher', 
-    'MetricFetcher', 'MetricFetcherToFile', 'SarimaModelFetcher'
+    'MetricFetcher', 'MetricFetcherToFile', 'SarimaModelFetcher',
+    'SoiaMetricsFetcher'
 ]
